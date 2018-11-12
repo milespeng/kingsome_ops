5bc2fc27ab5448536e878bf4e8396ca1dfc051e0
pengtao <pengtao@kingsome.cn>
2018-11-12 20:43:13 +0800

    数据库连接配置

--- ops/cmdb/models.py
+++ ops/cmdb/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 # Create your models here.
-
+# run python manage.py  makemigrations  cmdb
+# py manage.py migrate
 class Machine(models.Model):
     machine_id = models.IntegerField(auto_created=1)
     machine_name = models.CharField(max_length=200)
     public_ip = models.CharField(max_length=40)
     priv_ip = models.CharField(max_length=40)
-    create_time = models.DateTimeField()
+    create_time = models.DateTimeField('创建时间', auto_now=True)
