4884d336d9b350e873e34c054aa596b85e3bee0d
pengtao <pengtao@kingsome.cn>
2018-11-12 21:01:14 +0800

    修正错误

--- ops/cmdb/apps.py
+++ ops/cmdb/apps.py
@@ -5,6 +5,9 @@ class CmdbConfig(AppConfig):
     name = 'cmdb'
     DATABASES = {'default': {'ENGINE': 'django.db.backends.mysql', 'NAME': 'mydjango',
                              'USER': os.environ.get('MYSQL_USER', None), 'PASSWORD': os.environ.get('MYSQL_PSWD', None),
-                             "HOST": '192.168.100.30', 'PORT': 3306, }}
+                             "HOST": '192.168.100.30', 'PORT': 3306,
+                             'OPTIONS':{'isolation_level': None}
+                             }
+                 }
     LANGUAGE_CODE = 'zh-Hans'
     TIME_ZONE = 'Asia/Shanghai'
--- ops/cmdb/models.py
+++ ops/cmdb/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 # Create your models here.
 # run python manage.py  makemigrations  cmdb
-# py manage.py migrate
+# python manage.py migrate
+# python manage.py createsuperuser
+
 class Machine(models.Model):
     machine_id = models.IntegerField(auto_created=1)
     machine_name = models.CharField(max_length=200)
     public_ip = models.CharField(max_length=40)
     priv_ip = models.CharField(max_length=40)
     create_time = models.DateTimeField('创建时间', auto_now=True)
+
+    def __str__(self):
+        return self.machine_name
+
