7d3d7dd236ea01f97a0fff25505763c2c77922fd
pengtao <pengtao@kingsome.cn>
2018-11-12 17:05:31 +0800

    测试环境输出

--- ops/ops/urls.py
+++ ops/ops/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path('cmdb/', include('cmdb.urls')),
+               path('admin/', admin.site.urls), ]
