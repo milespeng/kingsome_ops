8d5cc006bca041758f12ea9b7605cc37778c47d0
miles <pt_yc@163.com>
2018-11-14 16:07:07 +0800

    change db to mysql

--- ops/ops/settings.py
+++ ops/ops/settings.py
@@ -74,13 +74,20 @@ WSGI_APPLICATION = 'ops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {'default': {'ENGINE': 'django.db.backends.mysql', 'NAME': 'mydjango',
+                         'USER': os.environ.get('MYSQL_USER', None), 'PASSWORD': os.environ.get('MYSQL_PSWD', None),
+                         "HOST": '192.168.100.30', 'PORT': 3306,
+                         'OPTIONS': {'isolation_level': None}
+                         }
+             }
+LANGUAGE_CODE = 'zh-Hans'
+TIME_ZONE = 'Asia/Shanghai'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
